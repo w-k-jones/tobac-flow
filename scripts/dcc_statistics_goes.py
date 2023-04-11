4c60f3713dfea4bad38e99d826bec0c91e4a4e4b
William Jones <william.jones@physics.ox.ac.uk>
2023-04-11 21:53:51 +0100

    Filter step labels than don't correspond with core/anvil labels

--- scripts/dcc_statistics_goes.py
+++ scripts/dcc_statistics_goes.py
@@ -124,6 +124,25 @@ for var, dtype in output_dtypes.items():
 
 # Filter invalid anvils and cores from dataset
 
+# Filter out cores/anvils or steps which don't appear in the others index
+
+wh_core = np.isin(dataset.core, dataset.core_step_core_index)
+wh_core_step = np.isin(dataset.core_step_core_index, dataset.core)
+wh_anvil = np.logical_and(
+    np.isin(dataset.anvil, dataset.thick_anvil_step_anvil_index),
+    np.isin(dataset.anvil, dataset.thin_anvil_step_anvil_index),
+)
+wh_thick_anvil_step = np.isin(dataset.thick_anvil_step_anvil_index, dataset.anvil)
+wh_thin_anvil_step = np.isin(dataset.thin_anvil_step_anvil_index, dataset.anvil)
+
+dataset = dataset.sel(
+    core=dataset.core.data[wh_core],
+    anvil=dataset.anvil.data[wh_anvil],
+    core_step=dataset.core_step.data[wh_core_step],
+    thick_anvil_step=dataset.thick_anvil_step[wh_thick_anvil_step],
+    thin_anvil_step=dataset.thin_anvil_step[wh_thin_anvil_step],
+)
+
 # Anvil NaN filter
 def any_nan(x, *args, **kwargs):
     return np.any(np.isnan(x))
