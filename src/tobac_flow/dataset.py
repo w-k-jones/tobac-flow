4875e72288f2311695594cec36744e4e7445b722
William Jones <william.jones@physics.ox.ac.uk>
2023-04-21 22:51:43 +0100

    Fix typo

--- src/tobac_flow/dataset.py
+++ src/tobac_flow/dataset.py
@@ -608,26 +608,28 @@ def flag_edge_labels(dataset, start_date, end_date):
     else:
         thin_anvil_edge_label_flag.loc[thin_anvil_edge_labels] = True
 
-    thin_anvil_start_labels = np.unique(
-        dataset.thin_anvil_label.sel(t=slice(None, start_date))
-    )
     if get_datetime_from_coord(dataset.t)[0] < start_date:
-        thin_anvil_start_label_flag = xr.zeros_like(dataset.anvil, dtype=bool)
+        thin_anvil_start_labels = np.unique(
+            dataset.thin_anvil_label.sel(t=slice(None, start_date))
+        )
     else:
         thin_anvil_start_labels = np.unique(dataset.thick_anvil_label[0])
+
+    thin_anvil_start_label_flag = xr.zeros_like(dataset.anvil, dtype=bool)
 
     if thin_anvil_start_labels[0] == 0:
         thin_anvil_start_label_flag.loc[thin_anvil_start_labels[1:]] = True
     else:
         thin_anvil_start_label_flag.loc[thin_anvil_start_labels] = True
 
-    thin_anvil_end_labels = np.unique(
-        dataset.thin_anvil_label.sel(t=slice(end_date, None))
-    )
     if get_datetime_from_coord(dataset.t)[-1] < end_date:
-        thin_anvil_end_label_flag = xr.zeros_like(dataset.anvil, dtype=bool)
+        thin_anvil_end_labels = np.unique(
+            dataset.thin_anvil_label.sel(t=slice(end_date, None))
+        )
     else:
         thin_anvil_end_labels = np.unique(dataset.thick_anvil_label[-1])
+
+    thin_anvil_end_label_flag = xr.zeros_like(dataset.anvil, dtype=bool)
 
     if thin_anvil_end_labels[0] == 0:
         thin_anvil_end_label_flag.loc[thin_anvil_end_labels[1:]] = True
