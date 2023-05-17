857b951025ac38e92717fe1c70e6d622157cf01a
William Jones <william.jones@physics.ox.ac.uk>
2023-05-17 04:43:15 +0100

    Update setup to find modules in subdirectories

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
+import pathlib
+import numpy as np
 from Cython.Build import cythonize
-from os.path import splitext, basename
-from glob import glob
-import numpy
 
+cwd = pathlib.Path("./")
+src = cwd / "src"
+packages = find_packages(src)
+modules = sorted(list(src.glob("**/[:alpha:]*.py")))
+cython_modules = list(src.glob("**/*.pyx"))
 setup(
-    name='tobac flow',
-    version='1.7.4',
+    name="tobac-flow",
+    version="1.7.4",
     description="Detection and tracking of deep convective clouds in high time resolution geostationary satellite imagery",
-    url='',
-    author='William Jones',
-    author_email='william.jones@physics.ox.ac.uk',
-    license='BSD-3',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    url="",
+    author="William Jones",
+    author_email="william.jones@physics.ox.ac.uk",
+    license="BSD-3",
+    packages=packages,
+    package_dir={"": "src"},
+    py_modules=modules,
     include_package_data=True,
     install_requires=[],
-    ext_modules=cythonize("./src/tobac_flow/_watershed.pyx"),
-    include_dirs=[numpy.get_include()],
+    ext_modules=cythonize(cython_modules),
+    include_dirs=[np.get_include()],
     zip_safe=False
 )
