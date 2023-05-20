f84025ff1f55c8dfcf96412829051e089610027f
William Jones <william.jones@physics.ox.ac.uk>
2023-05-20 17:59:24 +0100

    Fix core anvil index in goes statistics

--- scripts/dcc_statistics_goes.py
+++ scripts/dcc_statistics_goes.py
@@ -149,10 +149,10 @@ dataset = process_thick_anvil_properties(dataset)
 dataset = process_thin_anvil_properties(dataset)
 
 dataset["core_has_anvil_flag"] = xr.DataArray(
-    np.isin(dataset.core_step_anvil_index, dataset.anvil), {"core": dataset.core}
+    np.isin(dataset.core_anvil_index, dataset.anvil), {"core": dataset.core}
 )
 
-dataset.core_step_anvil_index[np.logical_not(dataset.core_has_anvil_flag)] = 0
+dataset.core_anvil_index[np.logical_not(dataset.core_has_anvil_flag)] = 0
 
 dataset["anvil_core_count"] = counts_groupby(dataset.core_anvil_index, dataset.anvil)
 
