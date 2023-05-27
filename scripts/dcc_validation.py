cee19422cd3d7801a47e99cc856812987f505437
William Jones <william.jones@physics.ox.ac.uk>
2023-05-27 12:18:55 +0100

    Update validation script

--- scripts/dcc_validation.py
+++ scripts/dcc_validation.py
@@ -1,4 +1,4 @@
-import os
+import pathlib
 from datetime import datetime
 import numpy as np
 import pandas as pd
@@ -14,9 +14,25 @@ parser.add_argument("file", help="File to validate", type=str)
 parser.add_argument(
     "-margin", help="Tolerance margin for validation (in pixels)", default=10, type=int
 )
+parser.add_argument(
+    "-time_margin",
+    help="Tolerance margin for validation (in time steps)",
+    default=3,
+    type=int,
+)
 parser.add_argument("-gd", help="GOES directory", default="../data/GOES16", type=str)
 parser.add_argument(
-    "-sd", help="Directory to save output files", default="../data/dcc_detect", type=str
+    "-glmsd",
+    help="Directory to save gridded glm files",
+    default="../data/glm_grid",
+    type=str,
+)
+
+parser.add_argument(
+    "-sd",
+    help="Directory to save output files",
+    default="../data/dcc_validation",
+    type=str,
 )
 parser.add_argument("-cglm", help="clobber existing glm files", action="store_true")
 
@@ -24,30 +40,36 @@ args = parser.parse_args()
 
 file = args.file
 margin = args.margin
+time_margin = args.time_margin
 clobber_glm = args.cglm
 
-goes_data_path = args.gd
-if not os.path.isdir(goes_data_path):
+goes_data_path = pathlib.path(args.gd)
+if not goes_data_path.exists():
     try:
-        os.makedirs(goes_data_path)
+        goes_data_path.mkdir()
     except (FileExistsError, OSError):
         pass
 
-save_dir = args.sd
-# if args.extend_path:
-# save_dir = os.path.join(save_dir, start_date.strftime('%Y/%m/%d'))
-if not os.path.isdir(save_dir):
+glm_save_dir = pathlib.path(args.glmsd)
+if not glm_save_dir.exists():
     try:
-        os.makedirs(save_dir)
+        glm_save_dir.mkdir()
+    except (FileExistsError, OSError):
+        pass
+
+save_dir = pathlib.path(args.sd)
+if not save_dir.exists():
+    try:
+        save_dir.mkdir()
     except (FileExistsError, OSError):
         pass
 
 # def validation(file, margin, goes_data_path, save_dir):
-if True:
+def main():
     """
     Validation process for detected DCCs in the given file
     """
-    from tobac_flow import io, abi, glm
+    from tobac_flow import io, glm
     from tobac_flow.dataset import (
         create_new_goes_ds,
         add_dataarray_to_ds,
@@ -67,17 +89,15 @@ if True:
         detection_ds.t.data[0], detection_ds.t.data[-1], freq="H", closed="left"
     ).to_pydatetime()
 
-    glm_save_name = "gridded_glm_flashes_%s.nc" % (dates[0].strftime("%Y%m%d_%H0000"))
-    glm_save_path = os.path.join(save_dir, glm_save_name)
-    validation_save_name = "validation_dccs_%s.nc" % (
-        dates[0].strftime("%Y%m%d_%H0000")
-    )
-    validation_save_path = os.path.join(save_dir, validation_save_name)
+    glm_save_name = f'gridded_glm_flashes_{dates[0].strftime("%Y%m%d_%H0000")}.nc'
+    glm_save_path = glm_save_dir / glm_save_name
+    validation_save_name = f'validation_dccs_{dates[0].strftime("%Y%m%d_%H0000")}.nc'
+    validation_save_path = save_dir / validation_save_name
 
     """
     Start validation
     """
-    if os.path.exists(glm_save_path) and not clobber_glm:
+    if glm_save_path.exists() and not clobber_glm:
         print(datetime.now(), "Loading from %s" % (glm_save_path), flush=True)
         gridded_flash_ds = xr.open_dataset(glm_save_path)
         glm_grid = gridded_flash_ds.glm_flashes
@@ -140,15 +160,15 @@ if True:
 
     # Create an array to filter objects near to boundaries
     edge_filter_array = np.full(marker_distance.shape, 1).astype("bool")
-    edge_filter_array[:3] = 0
-    edge_filter_array[-3:] = 0
-    edge_filter_array[:, :10] = 0
-    edge_filter_array[:, -10:] = 0
-    edge_filter_array[:, :, :10] = 0
-    edge_filter_array[:, :, -10:] = 0
+    edge_filter_array[:time_margin] = 0
+    edge_filter_array[-time_margin:] = 0
+    edge_filter_array[:, :margin] = 0
+    edge_filter_array[:, -margin:] = 0
+    edge_filter_array[:, :, :margin] = 0
+    edge_filter_array[:, :, -margin:] = 0
 
     # Filter objects near to missing glm data
-    wh_missing_glm = ndi.binary_dilation(glm_grid == -1, iterations=3)
+    wh_missing_glm = ndi.binary_dilation(glm_grid == -1, iterations=time_margin)
     edge_filter_array[wh_missing_glm] = 0
 
     flash_distance_to_marker = np.repeat(
@@ -171,9 +191,6 @@ if True:
             np.histogram(flash_distance_to_marker, bins=40, range=[0, 40])[0]
             / n_glm_in_margin
         )
-        # wvd_pod = np.sum(flash_distance_to_wvd<=10)/n_glm_in_margin
-        # wvd_pod_hist = np.histogram(flash_distance_to_wvd, bins=40,
-        #                             range=[0,40])[0] / n_glm_in_margin
         anvil_pod = np.sum(flash_distance_to_anvil <= 10) / n_glm_in_margin
         anvil_pod_hist = (
             np.histogram(flash_distance_to_anvil, bins=40, range=[0, 40])[0]
@@ -182,8 +199,6 @@ if True:
     else:
         growth_pod = np.float64(np.nan)
         growth_pod_hist = np.zeros([40])
-        # wvd_pod = np.float64(np.nan)
-        # wvd_pod_hist = np.zeros([40])
         anvil_pod = np.float64(np.nan)
         anvil_pod_hist = np.zeros([40])
 
@@ -209,19 +224,6 @@ if True:
     else:
         growth_far = np.float64(np.nan)
         growth_far_hist = np.zeros([40])
-
-    # wvd_margin_flag = apply_func_to_labels(detection_ds.wvd_label.data,
-    #                                        edge_filter_array, np.nanmin).astype('bool')
-    # n_wvd_in_margin = np.sum(wvd_margin_flag)
-    # wvd_min_distance = get_min_dist_for_objects(glm_distance, detection_ds.wvd_label.data)[0]
-    #
-    # if n_wvd_in_margin>0:
-    #     wvd_far = np.sum(wvd_min_distance[wvd_margin_flag]>10) / n_wvd_in_margin
-    #     wvd_far_hist = np.histogram(wvd_min_distance[wvd_margin_flag], bins=40,
-    #                             range=[0,40])[0] / n_wvd_in_margin
-    # else:
-    #     wvd_far = np.float64(np.nan)
-    #     wvd_far_hist = np.zeros([40])
 
     anvil_margin_flag = apply_func_to_labels(
         detection_ds.thick_anvil_label.data, edge_filter_array, np.nanmin
@@ -249,11 +251,6 @@ if True:
     print("POD =", growth_pod, flush=True)
     print("FAR = ", growth_far, flush=True)
 
-    # print('WVD:', flush=True)
-    # print('n =', n_wvd_in_margin, flush=True)
-    # print('POD =', wvd_pod, flush=True)
-    # print('FAR = ', wvd_far, flush=True)
-
     print("anvil:", flush=True)
     print("n =", n_anvil_in_margin, flush=True)
     print("POD =", anvil_pod, flush=True)
@@ -276,9 +273,6 @@ if True:
         ),
         validation_ds,
     )
-    # add_dataarray_to_ds(create_dataarray(flash_distance_to_wvd, ('flash',), "flash_wvd_distance",
-    #                                      long_name="closest distance from flash to detected wvd region",
-    #                                      dtype=np.float32), validation_ds)
     add_dataarray_to_ds(
         create_dataarray(
             flash_distance_to_anvil,
@@ -370,28 +364,6 @@ if True:
         ),
         validation_ds,
     )
-    # wvd validation
-    # add_dataarray_to_ds(create_dataarray(wvd_min_distance, ('wvd',), "wvd_glm_distance",
-    #                                      long_name="closest distance from wvd to GLM flash",
-    #                                      dtype=np.float32), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(wvd_margin_flag, ('wvd',), "wvd_margin_flag",
-    #                                      long_name="margin flag for wvd",
-    #                                      dtype=bool), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(wvd_far_hist, ('bins',), "wvd_far_histogram",
-    #                                      long_name="FAR histogram for wvds",
-    #                                      dtype=np.float32), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(wvd_pod_hist, ('bins',), "wvd_pod_histogram",
-    #                                      long_name="POD histogram for wvds",
-    #                                      dtype=np.float32), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(wvd_pod, tuple(), "wvd_pod",
-    #                                      long_name="POD for wvds",
-    #                                      dtype=np.float32), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(wvd_far, tuple(), "wvd_far",
-    #                                      long_name="FAR for wvds",
-    #                                      dtype=np.float32), validation_ds)
-    # add_dataarray_to_ds(create_dataarray(n_wvd_in_margin, tuple(), "wvd_count",
-    #                                      long_name="total number of wvds inside margin",
-    #                                      dtype=np.int32), validation_ds)
     # growth validation
     add_dataarray_to_ds(
         create_dataarray(
@@ -459,5 +431,6 @@ if True:
     print(datetime.now(), "Saving to %s" % (validation_save_path), flush=True)
     validation_ds.to_netcdf(validation_save_path)
 
-# if __name__=='__main__':
-#     validation(file, margin, goes_data_path, save_dir)
+
+if __name__ == "__main__":
+    main()
