eb58666050037792a762896535bf00624fd568ab
William Jones <william.jones@physics.ox.ac.uk>
2023-06-21 17:33:20 +0100

    Add get_mean_object_azimuth_and_speed function

--- tobac_flow/utils/geo_utils.py
+++ tobac_flow/utils/geo_utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import xarray as xr
+from scipy.stats import circmean
 from pyproj import Geod
+
+GEO = Geod(ellps="WGS84")
 
 
 def get_grid_spacing_from_lat_lon(
@@ -9,10 +12,9 @@ def get_grid_spacing_from_lat_lon(
     """
     Get the grid spacing of each pixel from 2d latitude and longitude arrays
     """
-    g = Geod(ellps="WGS84")
     dy, dx = np.zeros(lat.shape, dtype=float), np.zeros(lat.shape, dtype=float)
-    dy[:-1] = g.inv(lon[:-1], lat[:-1], lon[1:], lat[1:])[-1] / 1e3
-    dx[:, :-1] = g.inv(lon[:, :-1], lat[:, :-1], lon[:, 1:], lat[:, 1:])[-1] / 1e3
+    dy[:-1] = GEO.inv(lon[:-1], lat[:-1], lon[1:], lat[1:])[-1] / 1e3
+    dx[:, :-1] = GEO.inv(lon[:, :-1], lat[:, :-1], lon[:, 1:], lat[:, 1:])[-1] / 1e3
     dy[1:] += dy[:-1]
     dy[1:-1] /= 2
     dx[:, 1:] += dx[:, :-1]
@@ -57,8 +59,34 @@ def add_area_to_dataset(dataset: xr.Dataset, squeeze: bool = False) -> xr.Datase
     return dataset
 
 
+def get_mean_object_azimuth_and_speed(
+    lons: np.ndarray[float], lats: np.ndarray[float], t: np.ndarray[np.datetime64]
+) -> tuple[float, float]:
+    """
+    Given array of longitude, latitude and time, find the average direction of
+    propagation and speed
+    """
+    sort_args = np.argsort(t)
+    lifetime_seconds = np.diff(t[sort_args]).astype(int) / 1e9
+    azimuths, _, distances = GEO.inv(
+        lons[sort_args][:-1],
+        lats[sort_args][:-1],
+        lons[sort_args][1:],
+        lats[sort_args][1:],
+    )
+    speeds = distances / lifetime_seconds
+    wh_finite = np.logical_and(np.isfinite(azimuths), np.isfinite(speeds))
+    if np.any(wh_finite):
+        return circmean(azimuths[wh_finite], high=180, low=-180), np.mean(
+            speeds[wh_finite]
+        )
+    else:
+        return np.nan, np.nan
+
+
 __all__ = (
     "get_grid_spacing_from_lat_lon",
     "get_area_from_lat_lon",
     "add_area_to_dataset",
+    "get_mean_object_azimuth_and_speed",
 )
