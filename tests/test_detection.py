a0ab1d2cef7d5daf3c4c94d1ba1b090f7cf6f136
William Jones <william.jones@physics.ox.ac.uk>
2023-10-11 16:49:02 +0100

    Add tests for get_watershed_mask

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -4,6 +4,35 @@ import numpy as np
 from tobac_flow.flow import Flow
 
 
+def test_get_watershed_mask():
+    from tobac_flow.detection import get_watershed_mask
+
+    test_field = np.zeros([1, 5, 5], dtype=np.float32)
+    test_field[:, 3:] = 1
+
+    test_result = get_watershed_mask(test_field)
+
+    assert np.all(test_result[:, :2] == True)
+    assert np.all(test_result[:, 2:] == False)
+
+    test_result = get_watershed_mask(test_field, erode_distance=2)
+
+    assert np.all(test_result[:, :1] == True)
+    assert np.all(test_result[:, 1:] == False)
+
+    test_result = get_watershed_mask(test_field, erode_distance=3)
+
+    assert np.all(test_result == False)
+
+    # Test with missing values
+    test_field[:, 2] = np.nan
+
+    test_result = get_watershed_mask(test_field, erode_distance=1)
+
+    assert np.all(test_result[:, :3] == True)
+    assert np.all(test_result[:, 3:] == False)
+
+
 def test_get_combined_edge_field():
     from tobac_flow.detection import get_combined_edge_field
 
