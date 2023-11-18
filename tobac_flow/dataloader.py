53a9f8a783c732328e102f75aae99b80f367f080
William Jones <william.jones@physics.ox.ac.uk>
2023-11-18 14:45:35 +0000

    Fix area calculation in seviri_nat_dataloader

--- tobac_flow/dataloader.py
+++ tobac_flow/dataloader.py
@@ -791,7 +791,7 @@ def seviri_nat_dataloader(
             new_ds,
         )
 
-        area = get_pixel_area(ds.latitude.values, ds.longitude.values)
+        area = get_pixel_area(new_ds.latitude.values, new_ds.longitude.values)
 
         add_dataarray_to_ds(
             create_dataarray(
