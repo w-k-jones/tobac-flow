a82baafec965f6036709d153fe409a8d37078f95
William Jones <william.jones@physics.ox.ac.uk>
2023-11-19 08:00:41 +0000

    Revert c52e958

--- tobac_flow/_watershed.pyx
+++ tobac_flow/_watershed.pyx
@@ -186,7 +186,7 @@ cdef inline double _euclid_dist(Py_ssize_t pt0, Py_ssize_t pt1,
 @cython.boundscheck(False)
 @cython.cdivision(True)
 @cython.unraisable_tracebacks(False)
-cdef inline DTYPE_BOOL_t _diff_neighbors(cnp.intp_t[::1] output,
+cdef inline DTYPE_BOOL_t _diff_neighbors(DTYPE_INT32_t[::1] output,
                                          cnp.intp_t[::1] structure,
                                          DTYPE_BOOL_t[::1] mask,
                                          Py_ssize_t index) nogil:
@@ -197,7 +197,7 @@ cdef inline DTYPE_BOOL_t _diff_neighbors(cnp.intp_t[::1] output,
     """
     cdef:
         Py_ssize_t i, neighbor_index
-        cnp.intp_t neighbor_label0, neighbor_label1
+        DTYPE_INT32_t neighbor_label0, neighbor_label1
         Py_ssize_t nneighbors = structure.shape[0]
 
     if not mask[index]:
@@ -228,7 +228,7 @@ def watershed_raveled(cnp.float64_t[::1] image,
                       DTYPE_BOOL_t[::1] mask,
                       cnp.intp_t[::1] strides,
                       cnp.double_t compactness,
-                      cnp.intp_t[::1] output,
+                      DTYPE_INT32_t[::1] output,
                       DTYPE_BOOL_t wsl):
     """Perform watershed algorithm using a raveled image and neighborhood.
     Parameters
