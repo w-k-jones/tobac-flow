4be72088164a0de79c7d9bd5979acc7729c7884b
William Jones <william.jones@physics.ox.ac.uk>
2023-11-24 13:47:59 +0000

    Update seviri native postprocessing script

--- scripts/postprocess_seviri_nat_dcc.py
+++ scripts/postprocess_seviri_nat_dcc.py
@@ -5,15 +5,14 @@ import argparse
 import pathlib
 from datetime import datetime, timedelta
 from dateutil.parser import parse as parse_date
-from tobac_flow.analysis import get_label_stats
+from tobac_flow.analysis import get_label_stats, weighted_statistics_on_labels
 from tobac_flow.dataset import calculate_label_properties
-from tobac_flow.dataloader import find_seviri_files
 from tobac_flow.postprocess import (
     add_weighted_stats_to_dataset,
     add_cre_to_dataset,
     add_weighted_proportions_to_dataset,
 )
-from tobac_flow.utils import add_area_to_dataset
+from tobac_flow.utils.xarray_utils import add_dataarray_to_ds
 
 parser = argparse.ArgumentParser(
     description="""Validate detected DCCs using GOES-16 GLM data"""
@@ -39,21 +38,84 @@ def main():
         save_dir.mkdir()
 
     save_path = save_dir / fname.name
-    print("Saving to:", save_path)
+    print("Saving to:", save_path, flush=True)
 
+    print(datetime.now(), "Loading dataset from:", fname, flush=True)
     dataset = xr.open_dataset(fname)
+
+    dataset = dataset.drop_vars(
+        [
+            coord
+            for coord in dataset.coords
+            if coord
+            not in [
+                "t",
+                "y",
+                "x",
+                "core",
+                "anvil",
+                "core_step",
+                "thick_anvil_step",
+                "thin_anvil_step",
+            ]
+        ],
+        errors="ignore",
+    )
 
     start_date = parse_date((str(fname)).split("_S")[-1].split("_E")[0], fuzzy=True)
     end_date = parse_date((str(fname)).split("_E")[-1].split("_X")[0], fuzzy=True)
 
+    print(datetime.now(), "Calculating label properties", flush=True)
     calculate_label_properties(dataset)
 
     if args.save_spatial_props:
+        print(datetime.now(), "Calculating spatial properties", flush=True)
         get_label_stats(dataset.core_label, dataset)
         get_label_stats(dataset.thick_anvil_label, dataset)
         get_label_stats(dataset.thin_anvil_label, dataset)
 
+    print(datetime.now(), "Calculating pixel weights", flush=True)
     weights = np.repeat(dataset.area.data[np.newaxis, ...], dataset.t.size, 0)
+
+    for field in (dataset.BT,):
+        [
+            add_dataarray_to_ds(da[dataset.core_step.data - 1], dataset)
+            for da in weighted_statistics_on_labels(
+                dataset.core_step_label,
+                field.compute(),
+                weights,
+                name="core_step",
+                dim="core_step",
+                dtype=np.float32,
+            )
+        ]
+
+        [
+            add_dataarray_to_ds(da[dataset.thick_anvil_step.data - 1], dataset)
+            for da in weighted_statistics_on_labels(
+                dataset.thick_anvil_step_label,
+                field.compute(),
+                weights,
+                name="thick_anvil_step",
+                dim="thick_anvil_step",
+                dtype=np.float32,
+            )
+        ]
+
+        [
+            add_dataarray_to_ds(da[dataset.thin_anvil_step.data - 1], dataset)
+            for da in weighted_statistics_on_labels(
+                dataset.thin_anvil_step_label,
+                field.compute(),
+                weights,
+                name="thin_anvil_step",
+                dim="thin_anvil_step",
+                dtype=np.float32,
+            )
+        ]
+
+    # Remove BT to reduce file size
+    dataset = dataset.drop_vars("BT")
 
     # Load cloud properties file
     print(datetime.now(), "Loading cloud properties", flush=True)
@@ -84,6 +146,7 @@ def main():
             [],
         )
     )
+    print("Files found:", len(cld_files), flush=True)
 
     cld_ds = (
         xr.open_mfdataset(
@@ -105,13 +168,16 @@ def main():
     cld_ds.coords["t"] = dataset.t.sel(
         t=cld_dates, method="nearest", tolerance=timedelta(seconds=2)
     )
-    cld_ds = cld_ds.reindex(t=dataset.t)
+    if cld_ds.t.size != dataset.t.size:
+        print("Reindexing cloud dataset", flush=True)
+        cld_ds = cld_ds.reindex(t=dataset.t)
 
     print(datetime.now(), "Processing cloud properties", flush=True)
-    cld_weights = np.copy(weights)
-    cld_weights[cld_ds.qcflag.to_numpy() != 0] = 0
-    cld_weights[cld_ds.cth.to_numpy() > 30] = 0
-    cld_weights = xr.DataArray(cld_weights, cld_ds.lat.coords, cld_ds.lat.dims)
+    weights = (
+        weights * (cld_ds.qcflag == 0).astype(float) * (cld_ds.cth < 30).astype(float)
+    )
+    weights = xr.DataArray(weights, cld_ds.lat.coords, cld_ds.lat.dims)
+    weights = weights.compute()
 
     for var in (
         "cot",
@@ -128,9 +194,7 @@ def main():
     ):
         print(f"Processing {var}", flush=True)
         for dim in ("core_step", "thick_anvil_step", "thin_anvil_step"):
-            dataset = add_weighted_stats_to_dataset(
-                dataset, cld_ds, cld_weights, var, dim
-            )
+            dataset = add_weighted_stats_to_dataset(dataset, cld_ds, weights, var, dim)
 
     for var in ("lsflag", "lusflag", "illum", "cldtype", "phase"):
         print(f"Processing {var}", flush=True)
@@ -138,7 +202,7 @@ def main():
             dataset = add_weighted_proportions_to_dataset(
                 dataset,
                 cld_ds[var],
-                cld_weights,
+                weights,
                 dim,
             )
 
@@ -216,9 +280,7 @@ def main():
     ):
         print(f"Processing {var}", flush=True)
         for dim in ("core_step", "thick_anvil_step", "thin_anvil_step"):
-            dataset = add_weighted_stats_to_dataset(
-                dataset, flx_ds, cld_weights, var, dim
-            )
+            dataset = add_weighted_stats_to_dataset(dataset, flx_ds, weights, var, dim)
 
     print(datetime.now(), "Saving to %s" % (save_path), flush=True)
     # Add compression encoding
