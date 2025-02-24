6b94987915d82d603e681ec01a543533ac655e4b
William Jones <william.jones@physics.ox.ac.uk>
2025-02-24 18:24:09 +0000

    Return cooling rate values as K per minute

--- tobac_flow/utils/stats_utils.py
+++ tobac_flow/utils/stats_utils.py
@@ -316,7 +316,7 @@ def calc_max_cooling_rate(step_bt, step_t, t_steps=1):
 
 def cooling_rate_groupby(BT, times, groups, coord):
     return -xr.DataArray(
-        BT.assign_coords(t=times).groupby(groups).apply(lambda da : da.differentiate("t").min()).values,
+        BT.assign_coords(t=times).groupby(groups).apply(lambda da : da.differentiate("t").min()).values * 6e10,
         {coord.name: coord},
     )
 
